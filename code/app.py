88568e2c6eba81ee59ec801640afca9b87c0e1f4
Ubuntu <ubuntu@ip-172-31-0-200.ca-central-1.compute.internal>
2021-11-03 13:33:01 +0000

    change

--- code/app.py
+++ code/app.py
@@ -4,13 +4,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-  	return render_template('index.html')
-
-@app.route('/submit', methods=['POST'])
-def submit():
-  name = request.form['name']
-  age = request.form['age']
-  return '你好，' + name + '， 你的年龄是:' + age
-
+    return 'hello!'
 if __name__ == '__main__':
     app.run(host="0.0.0.0")
